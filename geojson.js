32eba9db32b34a8f96cb4f1d724495ba067af940
Yunikabputri <114121837+Yunikabputri@users.noreply.github.com>
2022-11-14 12:36:06 +0700

    Initial commit

---
+++ geojson.js
@@ -0,0 +1,187 @@
+var padang = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36868954862501,
+            -0.896737462249888
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36874216974462,
+            -0.8964969380211727
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36852923847869,
+            -0.8962516049105318
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36885078361286,
+            -0.8959844869952605
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36938618822023,
+            -0.8973333055662778
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36960729071745,
+            -0.8975985556587176
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.36979463607798,
+            -0.8978574393762244
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.3699967164664,
+            -0.8981205325663666
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            100.37022999082376,
+            -0.8984155288280107
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              100.36878868203536,
+              -0.8960179133737114
+            ],
+            [
+              100.36852960056154,
+              -0.8962346693171952
+            ],
+            [
+              100.36870937138019,
+              -0.896451425247804
+            ],
+            [
+              100.3702235956348,
+              -0.898421828731685
+            ]
+          ],
+          "type": "LineString"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              [
+                100.36883069002738,
+                -0.8959247952506786
+              ],
+              [
+                100.36854221845186,
+                -0.8962297136220485
+              ],
+              [
+                100.37020711154446,
+                -0.8983971056365476
+              ],
+              [
+                100.36883069002738,
+                -0.8959247952506786
+              ]
+            ]
+          ],
+          "type": "Polygon"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              [
+                100.36852573436153,
+                -0.8962297136220485
+              ],
+              [
+                100.36852573436153,
+                -0.8984547928585869
+              ],
+              [
+                100.3702235956348,
+                -0.8984547928585869
+              ],
+              [
+                100.3702235956348,
+                -0.8962297136220485
+              ],
+              [
+                100.36852573436153,
+                -0.8962297136220485
+              ]
+            ]
+          ],
+          "type": "Polygon"
+        }
+      }
+    ]
+  }
